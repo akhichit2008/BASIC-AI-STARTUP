eadc4b57e5dcf96df50a880485c05493d569e607
Adithya Krishna.K <128960498+ThisIsAdithya@users.noreply.github.com>
2023-03-26 09:34:17 +0000

    Co-authored-by: AKHILESH J.C <akhichit2008@users.noreply.github.com>

---
+++ Startup/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+from Startup.main import *
+import os
+
+def create_app(test_config=None):
+    app = Flask(__name__,instance_relative_config=True)
+    '''app.config.from_mapping(
+        SECRET_KEY='dev2023',
+    )'''
+    if test_config is None:
+        app.config.from_pyfile('app_config.py',silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/home')
+    def home():
+        return render_template('main.html') 
+    @app.route('/login')
+    def login():
+        return "Hello Everybody"
+    
+    return app
