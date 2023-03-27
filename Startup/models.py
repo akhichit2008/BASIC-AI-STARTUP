0aa0179e8f01f75b7c5442ffadc8a91a394a6d1f
AKHILESH J.C <88311744+akhichit2008@users.noreply.github.com>
2023-03-27 14:49:52 +0530

    Create models.py

---
+++ Startup/models.py
@@ -0,0 +1,14 @@
+from Startup import db
+from Startup.main import *
+
+class User(db.model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(500),unique=True,nullable=False)
+    email = db.Column(db.String(100),unique=True,nullable=False)
+    '''creation = db.Column(db.DateTime(timezone=True),
+    server_default=func.now())'''
+    password = db.Column(db.String(80),nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
